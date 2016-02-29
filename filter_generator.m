a7f9f4e544ecc9dafdaac9026f06494d724604e6
unknown <mm5213@eews303a-024.ic.ac.uk>
2016-02-29 23:31:47 +0000

    matlab

---
+++ filter_generator.m
@@ -0,0 +1,34 @@
+
+fsamp = 8000;
+nyquist = fsamp/2;
+
+order = 4;
+fpass1 = 180/nyquist;
+fpass2 = 450/nyquist;
+stop_atten = 23;
+pass_ripple = 0.4;
+
+[b,a] = ellip(order/2, pass_ripple, stop_atten, [fpass1 fpass2], 'bandpass');
+
+% Save coefficients to a text file 'fir_coefs.txt'
+format long e;
+
+formatSpec = '%2.15e';
+coefs = num2str(b',formatSpec);
+coefs = cellstr(coefs);
+datab = strjoin(coefs', ', ');
+
+formatSpec = '%2.15e';
+coefs = num2str(a',formatSpec);
+coefs = cellstr(coefs);
+dataa = strjoin(coefs', ', ');
+
+fileID = fopen('H:\RTDSPlab\lab5\RTDSP\coef.txt', 'w');
+fprintf(fileID, 'double b[]={');
+fprintf(fileID,datab);
+fprintf (fileID, '};');
+fprintf(fileID, 'double a[]={');
+fprintf(fileID,dataa);
+fprintf (fileID, '};');
+fclose(fileID);
+
